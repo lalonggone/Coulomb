eda8bd74692beef6c0bc5fd525b40bdef8d3fc1b
hamza-Algohary <hamza.moza.mizo@gmail.com>
2025-02-05 18:27:35 +0200

    fixed script commit_manifest.sh

--- commit_manifest.sh
+++ commit_manifest.sh
@@ -1,4 +1,6 @@
 if [ "$1" = "" ]
+then
     echo "please specify a message"
     exit
+fi
 cd io.github.hamza_algohary.Coulomb/ && git add io.github.hamza_algohary.Coulomb.yaml && git commit -m "$1"
